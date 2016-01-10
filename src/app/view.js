560a0f80d1a79b439b628d90a4c396408ae432ff
Oskar Gustafsson <oskargustafsson88@gmail.com>
2016-01-10 22:30:27 +0100

    Save to localStorage

--- src/app/view.js
+++ src/app/view.js
@@ -2,22 +2,37 @@ define(function (require) {
   'use strict';
 
   var View = require('libs/bff/dev/view');
+  var ItemRecord = require('entities/item');
   var ItemList = require('entities/itemList');
   var templateHtml = require('text!./template.html');
   var NewItemView = require('./newItem/view');
   var ItemListView = require('./itemList/view');
   var ItemActionsView = require('./itemActions/view');
 
+  var LOCAL_STORAGE_NS = 'todos-bff';
+
   return View.prototype.makeSubclass({
 
     constructor: function () {
-      var itemsList = new ItemList();
+      this.itemsList = new ItemList();
 
       this.el = this.parseHtml(templateHtml);
 
-      this.addChild(new NewItemView(itemsList), this.el);
-      this.addChild(new ItemListView(itemsList), this.el);
-      this.addChild(new ItemActionsView(itemsList), this.el);
+      this.addChild(new NewItemView(this.itemsList), this.el);
+      this.addChild(new ItemListView(this.itemsList), this.el);
+      this.addChild(new ItemActionsView(this.itemsList), this.el);
+
+      var savedItemsStr = localStorage[LOCAL_STORAGE_NS];
+      savedItemsStr && this.itemsList.pushAll(JSON.parse(savedItemsStr).map(function (itemData) {
+        return new ItemRecord(itemData);
+      }));
+
+      this.listenTo(this.itemsList, 'change:length', this.saveToLocalStorage);
+      this.listenTo(this.itemsList, 'item:change', this.saveToLocalStorage);
+    },
+
+    saveToLocalStorage: function () {
+      localStorage[LOCAL_STORAGE_NS] = JSON.stringify(this.itemsList);
     },
 
   });
