88f34ea91d44e4719eb233cc401ac37d2df71bbf
Oskar Gustafsson <oskargustafsson88@gmail.com>
2016-01-11 00:59:14 +0100

    Filter selection

--- src/app/itemActions/view.js
+++ src/app/itemActions/view.js
@@ -4,8 +4,15 @@ define(function (require) {
   var View = require('libs/bff/dev/view');
   var makeTemplate = require('lodash/string/template');
   var templateHtml = require('text!./template.html');
+  var router = require('app/router');
 
   var template = makeTemplate(templateHtml);
+
+  var FILTERS = {
+    '': 'All',
+    'active': 'Active',
+    'completed': 'Completed'
+  };
 
   return View.prototype.makeSubclass({
 
@@ -18,6 +25,7 @@ define(function (require) {
 
       this.listenTo(itemList, 'change:length', this.render);
       this.listenTo(itemList, 'change:nCompleted', this.render);
+      this.listenTo(router, 'change:route', this.render);
     },
 
     getHtml: function () {
@@ -26,6 +34,8 @@ define(function (require) {
         nTodosLeft: nTodosLeft + ' item' + (nTodosLeft === 1 ? '' : 's') + ' left',
         isListEmpty: this.itemList.length === 0,
         isNoneCompleted: this.itemList.nCompleted === 0,
+        currentPath: router.route,
+        filters: FILTERS,
       });
     },
 
